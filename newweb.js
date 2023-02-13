6e866e78acd7bcef0d6fd605d170d3f3c636a22c
krish <114145459+krishkumar84@users.noreply.github.com>
2023-02-14 00:55:10 +0530

    Add files via upload

--- newweb.js
+++ newweb.js
@@ -3,3 +3,37 @@ const navLinks = document.getElementById('nav-links');
 toggleButton.addEventListener('click', () => {
     navLinks.classList.toggle('active');
 })
+
+    window.addEventListener('scroll', function() {
+      const target = document.querySelector('.scroll-on');
+      const targetPosition = target.getBoundingClientRect().top;
+      const windowHeight = window.innerHeight;
+    
+      if (targetPosition < windowHeight) {
+        target.classList.add('visible');
+      }
+      (function() {
+        let listenerAdded = false;
+        
+        const addListener = function() {
+          if (!listenerAdded) {
+            window.addEventListener('scroll', function() {
+              const targets = document.querySelectorAll('.scroll-on');
+              targets.forEach(function(target) {
+                const targetPosition = target.getBoundingClientRect().top;
+                const windowHeight = window.innerHeight;
+        
+                if (targetPosition < windowHeight) {
+                  target.classList.add('visible');
+                }
+              });
+            });
+        
+            listenerAdded = true;
+          }
+        };
+        
+        addListener();
+      })();
+    });
+    
